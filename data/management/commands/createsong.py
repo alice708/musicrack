36d116ef4a2f3c2a1b658d701101b01461fcb392
Alice <aliceray678@gmail.com>
2025-04-23 16:38:52 +0100

    Add prototype command for adding song and simplified field names

---
+++ data/management/commands/createsong.py
@@ -0,0 +1,21 @@
+import argparse
+from django.core.management.base import BaseCommand, CommandError
+from data.models import Artist,Album,Song
+
+class Command(BaseCommand):
+    help = "Closes the specified poll for voting"
+
+    def add_arguments(self, parser):
+        parser.add_argument("--name", required=True, type=str)
+        parser.add_argument("--length", required=True, type=int)
+
+
+    def handle(self, *args, **options):
+        self.stdout.write(
+                self.style.SUCCESS('You entered "%s"' % options["name"])
+            )
+        self.stdout.write(
+                self.style.SUCCESS('You entered "%s"' % options["length"])
+            )
+        song = Song.create(name = options["name"], length = options["length"])
+        song.save()
--- data/models.py
+++ data/models.py
@@ -1,15 +1,21 @@
 from django.db import models
 
 class Song(models.Model):
-    song_name = models.CharField(max_length=200)
-    song_length = models.IntegerField()
+    name = models.CharField(max_length=200)
+    length = models.IntegerField()
+
+    @classmethod
+    def create(cls, name, length):
+        song = cls(name=name, length=length)
+        # do something with the book
+        return song
 
 class Album(models.Model):
-    album_name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
     year_released = models.IntegerField()
     song = models.ForeignKey(Song, on_delete=models.CASCADE)
 
 class Artist(models.Model):
-    artist_name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
     genre = models.CharField(max_length=200)
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
