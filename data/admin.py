06878bd2ce042e9d174d07cf90eea5f1c72c1100
Alice <aliceray678@gmail.com>
2025-04-25 13:53:52 +0100

    added basic search functionality

--- data/admin.py
+++ data/admin.py
@@ -6,16 +6,19 @@ from .models import Artist,Album,Song
 class ArtistAdmin(admin.ModelAdmin):
     fields = ["name", "genre", "id"]
     list_display = ["name", "genre"]
+    search_fields = ["name", "genre"]
 
 admin.site.register(Artist, ArtistAdmin)
 
 class AlbumAdmin(admin.ModelAdmin):
     fields = ["name", "year_released", "id", "artist"]
     list_display = ["name", "year_released", "artist"]
+    search_fields = ["name", "year_released", "artist__name"]
 admin.site.register(Album, AlbumAdmin)
 
 class SongAdmin(admin.ModelAdmin):
     fields = ["name", "length", "id", "album"]
     list_display = ["name", "length", "album"]
+    search_fields = ["name", "length", "album__name"]
 admin.site.register(Song, SongAdmin)
 
