2c4c55c015bd5b69091019afc281ba07b47885cc
Alice <aliceray678@gmail.com>
2025-04-25 11:55:52 +0100

    switch direction of many to one relationships

--- data/management/commands/createsong.py
+++ data/management/commands/createsong.py
@@ -9,14 +9,9 @@ class Command(BaseCommand):
         parser.add_argument("--id", required=True, type=str)
         parser.add_argument("--name", required=True, type=str)
         parser.add_argument("--length", required=True, type=str)
+        parser.add_argument("--album", required=True, type=str)
 
 
     def handle(self, *args, **options):
-        self.stdout.write(
-                self.style.SUCCESS('You entered "%s"' % options["name"])
-            )
-        self.stdout.write(
-                self.style.SUCCESS('You entered "%s"' % options["length"])
-            )
-        song = Song.create(id = options["id"], name = options["name"], length = options["length"])
+        song = Song.create(id = options["id"], name = options["name"], length = options["length"], album = options["album"])
         song.save()
--- data/models.py
+++ data/models.py
@@ -1,25 +1,25 @@
 from django.db import models
 
+class Artist(models.Model):
+    id = models.CharField(max_length=200, primary_key=True)
+    name = models.CharField(max_length=200)
+    genre = models.CharField(max_length=200)
+
+class Album(models.Model):
+    id = models.CharField(max_length=200, primary_key=True)
+    name = models.CharField(max_length=200)
+    year_released = models.IntegerField()
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+
 class Song(models.Model):
     id = models.CharField(max_length=200, primary_key=True)
     name = models.CharField(max_length=200)
     # Make this a custom field of 2 integers
     # Or make this 2 fields, mins/secs and display it as one?
     length = models.CharField(max_length=200)
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
 
     @classmethod
-    def create(cls, id, name, length):
-        song = cls(id=id, name=name, length=length)
+    def create(cls, id, name, length, album):
+        song = cls(id=id, name=name, length=length, album=album)
         return song
-
-class Album(models.Model):
-    id = models.CharField(max_length=200, primary_key=True)
-    name = models.CharField(max_length=200)
-    year_released = models.IntegerField()
-    song = models.ForeignKey(Song, on_delete=models.CASCADE)
-
-class Artist(models.Model):
-    id = models.CharField(max_length=200, primary_key=True)
-    name = models.CharField(max_length=200)
-    genre = models.CharField(max_length=200)
-    album = models.ForeignKey(Album, on_delete=models.CASCADE)
