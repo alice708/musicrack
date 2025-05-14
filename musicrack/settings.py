f6e42057250a8b6c24d845651750d93b830e15ea
Alice <aliceray678@gmail.com>
2025-05-14 14:00:13 +0100

    "typo"

--- musicrack/settings.py
+++ musicrack/settings.py
@@ -24,8 +24,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-
-# Load environment variables from .env file
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 
